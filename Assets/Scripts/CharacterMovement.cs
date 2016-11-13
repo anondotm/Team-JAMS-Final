581622e8456a7bbd7e370e0698a2a987a6db127e
Anan McHarris <am7155@nyu.edu>
2016-11-13 12:47:47 -0500

    Commit #1: Basic character controller movement/rotation.

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float playerSpeed;
+	CharacterController playerController;
+
+	// Use this for initialization
+	void Start () {
+		playerController = GetComponent<CharacterController> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.eulerAngles = new Vector3 (0, 0, 0);
+		}
+		else if (Input.GetKey (KeyCode.S)) {
+			transform.eulerAngles = new Vector3 (0, -180, 0);
+
+		}
+		else if (Input.GetKey (KeyCode.A)) {
+			transform.eulerAngles = new Vector3 (0, -90, 0);
+
+		}
+		else if (Input.GetKey (KeyCode.D)) {
+			transform.eulerAngles = new Vector3 (0, 90, 0);
+		}
+
+
+
+		playerController.SimpleMove (new Vector3(inputX,0,inputY) * playerSpeed);
+
+	}
+}
