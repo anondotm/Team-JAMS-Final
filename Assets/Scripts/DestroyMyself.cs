d08a6d9f70d065203ee05775e1d1d916d96fdda1
Junege Hong <jh4402@nyu.edu>
2016-11-19 14:52:14 -0500

    Added UI elements for health/waves, more difficult enemies spawn and scale with wave no. Transitions to game over scene if you lose.

--- Assets/Scripts/DestroyMyself.cs
+++ Assets/Scripts/DestroyMyself.cs
@@ -3,20 +3,69 @@ using System.Collections;
 
 public class DestroyMyself : MonoBehaviour {
 
+	HealthManager healthmanager; 
+	public GameObject gameManager; 
+
 	// Use this for initialization
 	void Start () {
-	
+
+		gameManager = GameObject.FindGameObjectWithTag ("gameManagertag"); 
+		healthmanager = gameManager.GetComponent<HealthManager> (); 
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+
+			Destruction (); 
+			Debug.Log ("this object is destroyed via keypress!"); 
+
+		}
 	
 	}
 
-	void OnBecameInvisible () {
+	void OnCollisionEnter(Collision c) {
+		if (c.gameObject.tag == "enemy") {
+			Debug.Log ("destruction via collision!"); 
+			Destruction (); 
+		}
 
+	}
+
+	void killMyself() {
+
+		if (transform.position.z >= 24) {
+			Debug.Log("destruction via position");
+			Destruction (); 
+
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if(other.gameObject.tag == "enemy") {
+			Debug.Log ("triggered");
+			Destruction (); 
+
+		}
+
+	}
+
+	void OnBecameInvisible() {
+
+		Debug.Log ("destruction via invisibility"); 
+		Destruction (); 
+	}
+
+
+	void Destruction () {
+
+		healthmanager.SetHealth (1); 
+		healthmanager.SetHealthText (); 
 		Destroy (this.gameObject); 
 		Debug.Log ("gone!"); 
-
 	}
 }
