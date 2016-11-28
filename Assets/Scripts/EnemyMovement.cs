70877d233269a070588209755c2f19dcb1b1ad37
Aleksei Waddington <amw694@nyu.edu>
2016-11-28 14:10:27 -0500

    Added the need to look at the enemies to understand which bullet is needed to destroy them.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,20 +7,47 @@ public class EnemyMovement : MonoBehaviour {
 	public string enemyIdentity;
 
 	public float speed;
+	Renderer enemyRend;
+	bool isCanonLooking = false;
 
 	void Start () {
-
+		enemyRend = GetComponent<Renderer> ();
 		speed = 1.5f; //+ PlayerPrefs.GetInt ("enemySpeedBonus"); //speed will increase every level
 	}
 
+	void Update (){
+		//If the Canon is not looking at the object, then set the color of the ennemy to grey.
+		if (!isCanonLooking) {
+			enemyRend.material.SetColor ("_Color", Color.grey);
+		}
+	}
 	// Update is called once per frame
 	void FixedUpdate () {
-
 		goToPlayer(); //call function 
-
 	}
 
 	void goToPlayer() { //makes enemies move down at the speed variable specified 
 		transform.position += -Vector3.forward *speed* Time.deltaTime;
+	}
+
+	void OnTriggerEnter( Collider canon){
+		isCanonLooking = true;
+		getColor ();
+	}
+	void OnTriggerStay( Collider canon){
+		isCanonLooking = true;
+		getColor ();
+	}
+	void OnTriggerExit( Collider canon){
+		isCanonLooking = false;
+	}
+	void getColor(){
+		if (enemyIdentity == "Red") {
+			enemyRend.material.SetColor ("_Color", Color.red);
+		} else if (enemyIdentity == "Blue") {
+			enemyRend.material.SetColor ("_Color", Color.blue);
+		} else {
+			enemyRend.material.SetColor ("_Color", Color.green);
+		}
 	}
 }
---
+++ Assets/Scripts/IgnoreBulletCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreBulletCollision : MonoBehaviour {
+	public GameObject CanonSight;
+	// Use this for initialization
+	void Start () {
+		CanonSight = GameObject.FindGameObjectWithTag ("aim");
+		Physics.IgnoreCollision (CanonSight.GetComponent<Collider> (), GetComponent<Collider> ());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
