636704b247bdcb1fb3eda96bd740677b1dfc0231
Anan McHarris <am7155@nyu.edu>
2016-11-29 00:26:33 -0500

    It's been a while since I pushed: - Added physical feedback for P2 "stacking" ammo - Tweaked spherecast to allow for picking up ammo from farther away - Added "ghost stack" to indicate to player when they can pick up ammo - Changed player speed based on amt of ammo held

--- Assets/Scripts/PickupAmmo.cs
+++ Assets/Scripts/PickupAmmo.cs
@@ -21,81 +21,123 @@ public class PickupAmmo : MonoBehaviour {
 	//UI text element displaying ammo held
 	public GameObject heldAmmoText;
 
+	//Array with positions to instantiate "physical" ammo
+	public GameObject physicalRed;
+	public GameObject physicalGreen;
+	public GameObject physicalBlue;
+	public Transform[] heldAmmoPosition;
+	public List<GameObject> physicalAmmoList = new List<GameObject>();
+
 
 	void Start () {
 		//stores intiial movement speed of character so we can manipulate it based on ammoHeld
-		//initialMoveSpeed = GetComponent<Movement> ().movespeed;
+		initialMoveSpeed = this.gameObject.GetComponent<CharacterMovement> ().playerSpeed;
+		currentMoveSpeed = initialMoveSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		if (Input.GetKey (KeyCode.F)) {
+			//instantiateAmmo ();
+		}
+
 		//create and project a spherecast
 		Ray ammoCheckRay = new Ray (transform.position, transform.forward);
 		RaycastHit hit;
-		if (Physics.SphereCast(ammoCheckRay,.5f, out hit, 2f)){
+		if (Physics.SphereCast (ammoCheckRay, .5f, out hit, 2.5f)) {
 			
 			//Debug.Log ("There's something there...");
 
-			if (Input.GetKeyDown (KeyCode.Space)) {
+			//if the raycast hits an ammo repository (checks if object has AmmoSource script
+			if (hit.collider.GetComponent<AmmoSource> () == true) {
 
-				//if the raycast hits an ammo repository (checks if object has AmmoSource script
-				if (hit.collider.GetComponent<AmmoSource>() == true) {
+				if (Input.GetKeyDown (KeyCode.Space)) {
+
 
 					//check if player is not holding 3+ ammo
 					if (heldAmmoSize < 3) {
 						
 						if (hit.collider.tag == "Ammo1") {
 							heldAmmo.Add ("Red");
+							instantiateAmmo (physicalRed);
 							updateText ();
 						} else if (hit.collider.tag == "Ammo2") {
 							heldAmmo.Add ("Green");
+							instantiateAmmo (physicalGreen);
 							updateText ();
 						} else if (hit.collider.tag == "Ammo3") {
 							heldAmmo.Add ("Blue");
+							instantiateAmmo (physicalBlue);
 							updateText ();
 						} 
 
 						//updateText ();
 
-						currentMoveSpeed -= initialMoveSpeed * .2f;
-						//GetComponent<Movement> ().movespeed = currentMoveSpeed;
-					
-					//if they are, tell them to drop something off!
-					} else {
-						Debug.Log ("Drop some stuff!");
+
+						currentMoveSpeed -= (initialMoveSpeed * .2f);
+						this.gameObject.GetComponent<CharacterMovement> ().playerSpeed = currentMoveSpeed;
+
 					}
 
+				} else {
+					if (heldAmmoSize < 3) {
+						heldAmmoPosition [heldAmmoSize].gameObject.SetActive (true);
+					}
 				}
 
-				//players can use the trash to drop off or "clear" held ammo
-				else if (hit.collider.tag == "Trash") {
-					heldAmmo.Clear ();
+				if (heldAmmoSize > 0) {
+					heldAmmoPosition [heldAmmo.Count - 1].gameObject.SetActive (false);
+				}
 
-					updateText ();
-
-					currentMoveSpeed = initialMoveSpeed;
-					GetComponent<Movement> ().movespeed = currentMoveSpeed;
+			}
 				
-					//when players interact with the dumbwaiter, the game actually just "adds" the player's ammo to the Cannon Object's ammo list ("cannonAmmo");
-				} else if (hit.collider.tag == "DumbWaiter") {
-					cannonObject.GetComponent<ShootBullet> ().cannonAmmo.AddRange (heldAmmo);
+
+			//players can use the trash to drop off or "clear" held ammo
+			else if (hit.collider.tag == "Trash") {
+
+				if (Input.GetKeyDown (KeyCode.Space)) {
 					heldAmmo.Clear ();
-
-					updateText ();
-
-					cannonObject.GetComponent<ShootBullet> ().textUpdate();
+					clearPhysicalAmmo ();
 
 					currentMoveSpeed = initialMoveSpeed;
-					//GetComponent<Movement> ().movespeed = currentMoveSpeed;
+					this.gameObject.GetComponent<CharacterMovement> ().playerSpeed = currentMoveSpeed;
+
+					updateText ();
 				}
 
-				//whenever you press space, updates the text
-				//updateText ();
 			}
 
+
+				
+			//when players interact with the dumbwaiter, the game actually just "adds" the player's ammo to the Cannon Object's ammo list ("cannonAmmo");
+			else if (hit.collider.tag == "DumbWaiter") {
+
+				if (Input.GetKeyDown (KeyCode.Space)) {
+					cannonObject.GetComponent<ShootBullet> ().cannonAmmo.AddRange (heldAmmo);
+					heldAmmo.Clear ();
+					clearPhysicalAmmo ();
+
+					currentMoveSpeed = initialMoveSpeed;
+					this.gameObject.GetComponent<CharacterMovement> ().playerSpeed = currentMoveSpeed;
+
+					updateText ();
+
+					cannonObject.GetComponent<ShootBullet> ().textUpdate ();
+
+				}
+
+			}
+
+			//whenever you press space, updates the text
+			//updateText ();
+		} else {
+			if (heldAmmoSize < 3) {
+				heldAmmoPosition [heldAmmoSize].gameObject.SetActive (false);
+			}
 		}
-	}
+
+		}
 
 	void FixedUpdate () {
 
@@ -114,5 +156,20 @@ public class PickupAmmo : MonoBehaviour {
 			}
 		}
 
+	}
+
+	public void instantiateAmmo(GameObject ammoType) {
+
+		GameObject newStack = (GameObject)Instantiate (ammoType, heldAmmoPosition [heldAmmoSize].position, Quaternion.identity);
+		newStack.transform.parent = this.transform;
+		physicalAmmoList.Add (newStack);
+
+	}
+
+	public void clearPhysicalAmmo () {
+		for (int i = 0; i < physicalAmmoList.Count; i++) {
+			Destroy (physicalAmmoList [i]);
+		}
+		physicalAmmoList.Clear ();
 	}
 }
