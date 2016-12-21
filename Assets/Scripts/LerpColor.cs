c1f08a3071cdbb3668ae863026e7af4298a3a7d4
Aleksei Waddington <Aleksei Waddington>
2016-12-21 18:53:17 -0500

    Finished working and implementing the canon. Tried to make it fit more with our theme. Also wanted it to feel more dynamic. Tried to make it look more part of a body with veins and stuff. Also changed the ammo indicators on the canon itself to make it pop out more, and feel better as well (rather than have it static as before). Also changed the texture for the tube.

--- Assets/Scripts/LerpColor.cs
+++ Assets/Scripts/LerpColor.cs
@@ -4,16 +4,22 @@ using System.Collections;
 public class LerpColor : MonoBehaviour {
 	Renderer bulletRend;
 	public Color lerpedColor = Color.white;
-	public Color bulletColor = Color.black;
+	Color bulletColor = Color.black;
 	void Start(){
 		bulletRend = GetComponent<Renderer> ();
 	}
 	void Update() {
 		lerpedColor = Color.Lerp(Color.black, bulletColor, Mathf.PingPong(Time.time, 1));
 		bulletRend.material.SetColor ("_Color", lerpedColor);
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			changeColorTest (Color.red);
+		}
 	}
 	public void LerpAmmoIndicatorColor(Color color, GameObject AmmoIndicator){
 		bulletColor = color;
 		bulletRend = AmmoIndicator.GetComponent<Renderer> ();
+	}
+	void changeColorTest(Color colour){
+		bulletColor = colour;
 	}
 }
