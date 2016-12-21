55e08cc5eaacd0ee6978f7f4c4ed0d8ce56bf792
Aleksei Waddington <Aleksei Waddington>
2016-12-21 17:08:55 -0500

    Added a new canon... still working on it.

---
+++ Assets/Scripts/LerpColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpColor : MonoBehaviour {
+	Renderer bulletRend;
+	public Color lerpedColor = Color.white;
+	public Color bulletColor = Color.black;
+	void Start(){
+		bulletRend = GetComponent<Renderer> ();
+	}
+	void Update() {
+		lerpedColor = Color.Lerp(Color.black, bulletColor, Mathf.PingPong(Time.time, 1));
+		bulletRend.material.SetColor ("_Color", lerpedColor);
+	}
+	public void LerpAmmoIndicatorColor(Color color, GameObject AmmoIndicator){
+		bulletColor = color;
+		bulletRend = AmmoIndicator.GetComponent<Renderer> ();
+	}
+}
